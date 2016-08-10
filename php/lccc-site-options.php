e86e5657b10b569b09f68795d4fb28f0ce30e3e5
Joe Querin <joecue@gmail.com>
2016-08-10 12:22:17 -0400

    Site Options Settings
    Added Footer Phone (lccc_footer_phone)
    Added Footer Email (lccc_footer_email)

--- php/lccc-site-options.php
+++ php/lccc-site-options.php
@@ -22,6 +22,8 @@ class new_lccc_base_path_setting {
   register_setting( 'general', 'lccc_base_path', 'esc_attr' );
   add_settings_section( 'lccc-settings', 'LCCC Settings', '__return_false', 'general' );
   add_settings_field( 'lccc_base_url_path', '<label for="lccc_base_path">'.__('Base URL Path:' , 'lccc_base_path').'</label>', array(&$this, 'lccc_fields_html') , 'general', 'lccc-settings' );
+  add_settings_field( 'lccc_footer_phone', '<label for="lccc_footer_phone">'.__('Phone Number:' , 'lccc_footer_phone').'</label>', array(&$this, 'lccc_footer_phone_field_html') , 'general', 'lccc-settings' );
+  add_settings_field( 'lccc_footer_email', '<label for="lccc_footer_email">'.__('Email Address:' , 'lccc_footer_email').'</label>', array(&$this, 'lccc_footer_email_field_html') , 'general', 'lccc-settings' );
  }
  
  function lccc_fields_html() {
@@ -30,6 +32,16 @@ class new_lccc_base_path_setting {
   echo '<p class="description" id="tagline-description">Enter the URL path <strong>(without starting or trailing /)</strong> to represent where this site exists in the website.</p>';
   echo '<p class="description" id="tagline-description"><strong>Example: student-resources/academic-resources/academic-divisions</strong></p>';
  }
- 
+
+
+ function lccc_footer_phone_field_html() {
+  $value = get_option( 'lccc_footer_phone', '' );
+  echo '<input type="text" id="lccc_footer_phone" name="lccc_footer_phone" value="' . $value . '" size="75" />';
+ }
+
+ function lccc_footer_email_field_html() {
+  $value = get_option( 'lccc_footer_email', '' );
+  echo '<input type="text" id="lccc_footer_email" name="lccc_footer_email" value="' . $value . '" size="75" />';
+ }
 }
 ?>
